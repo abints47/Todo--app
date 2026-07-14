c4daf2968648abd66843c5019f12035732eaabb2
AbinTS <abinthazhathupurakkal@gmail.com>
2026-07-14 14:38:20 +0530

    Configure Github Pages deployment

--- todo-app/next.config.ts
+++ todo-app/next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: 'export', // Tells Next.js to export static HTML/CSS/JS
+  images: {
+    unoptimized: true, // Required because GitHub Pages is static hosting
+  },
+  // GitHub Pages serves sites at: https://username.github.io/repo-name/
+  // basePath sets this subpath so links and assets don't break
+  basePath: isProd ? '/Todo--app' : '', 
 };
 
 export default nextConfig;
