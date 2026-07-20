99d5ee50af861c2e23416695f0a6d4f12e4183a3
Abin T S <abints30122004@gmail.com>
2026-07-20 14:39:07 +0530

    next.config error resolved

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,15 @@
-/** @type {import('next').NextConfig} */
+import type { NextConfig } from 'next';
+
 const isProd = process.env.NODE_ENV === 'production';
 
-const nextConfig = {
-  
+const nextConfig: NextConfig = {
+  output: 'export', // Required: Tells Next.js to produce a static HTML/CSS/JS export in the ./out folder
   images: {
     unoptimized: true, // Required because GitHub Pages is static hosting
   },
   // GitHub Pages serves sites at: https://username.github.io/repo-name/
   // basePath sets this subpath so links and assets don't break
-  basePath: isProd ? '/Todo--app' : '', 
+  basePath: isProd ? '/Todo--app' : '',
 };
 
 export default nextConfig;
